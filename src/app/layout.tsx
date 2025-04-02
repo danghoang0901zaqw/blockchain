9670a2bd51df2928788d12fb31bbb18d8e6fc500
Hoang Tran <hoang.tran@apps-cyclone.com>
2025-04-02 17:25:52 +0700

    feat(notify)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { WagmiProvider } from "./provider/WagmiProvider";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Toaster position="top-right" richColors expand={true} />
         <WagmiProvider>{children}</WagmiProvider>
       </body>
     </html>
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,4 +1,3 @@
-"use client"
 import { http, createConfig } from 'wagmi';
 import { base, mainnet, optimism } from 'wagmi/chains';
 import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors';
